d2d75f5155705072a6a4abafd0494fc1c7e3bcec
JusticeMuch <34315657+JusticeMuch@users.noreply.github.com>
2019-10-09 15:38:19 +0200

    Update hofstadter_mf.ml

--- day01/ex05/hofstadter_mf.ml
+++ day01/ex05/hofstadter_mf.ml
@@ -1,15 +1,19 @@
 let rec hfs_f n = 
-  if n == 0 then 
-    1
-  else if n > 0 then 
-    n - hsf_m (hsf_f (n - 1))
-  else
-    -1;;
+  if n == 0 then  1
+  else if n > 0 then n - hfs_m (hfs_f (n - 1))
+  else -1
+and hfs_m n = 
+  if n == 0 then 0
+  else if n > 0 then n - hfs_f (hfs_m (n - 1))
+  else -1;;
 
-let rec hfs_m n = 
-  if n == 0 then 
-    0
-  else if n > 0 then 
-    n - hsf_f (hsf_m (n - 1))
-  else
-    -1;;
+  let () = 
+    print_int(hfs_m 0);
+    print_string "\n";
+    print_int(hfs_f 0);
+    print_string "\n";
+    print_int(hfs_m 4);
+    print_string "\n";
+    print_int(hfs_f 4);
+    print_string "\n";
+    ;;
